aa4643ea7cd976dac758f38032996aa2340b7bf6
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-18 11:24:04 -0300

    Criada script final

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const fs = require('fs').promises;
+const { promises: fs } = require('fs')
+
+async function existsPath (directory) { 
+ try {
+  await fs.access(directory)
+  return true
+ } catch {
+  return false
+ }
+}
+
+async function listDirectoryFiles(directory, files) {
+  if(!files)
+    files = []
+
+  if(! await existsPath(directory)) {
+    console.log("Directory does not exist.")
+  } 
+  else {
+    console.log("Directory exists.")
+    let filesList = await fs.readdir(directory)
+    for(let k in filesList) {
+      let stat = await fs.stat(directory + '/' + filesList[k])
+      if(stat.isDirectory()) {
+        await listDirectoryFiles(directory + '/' + filesList[k], files)
+      } 
+      else {
+        files.push(directory + '/' + filesList[k])
+      }
+    }
+  }
+
+  return files
+}
+
+async function test() {
+  let files = await listDirectoryFiles('./data')
+  console.log(files)
+}
+
+test();
